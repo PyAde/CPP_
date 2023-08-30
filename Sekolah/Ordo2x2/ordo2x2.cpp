71b4a04aa3950af9ac348cd7511a76b6e78fd44a
PyAde <liodeo23@gmail.com>
2023-08-30 10:40:03 +0800

    Belajar CPP

--- Sekolah/Ordo2x2/ordo2x2.cpp
+++ Sekolah/Ordo2x2/ordo2x2.cpp
@@ -2,30 +2,42 @@
 
 using namespace std;
 
-float matrix2x2(float matrix[2][2]){
-	float det = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
-	return det;
+// Fungsi untuk mengisi matriks
+void isiMatriks(int matriks[][2], const string& nama) {
+    cout << "Masukkan elemen-elemen matriks " << nama << ":\n";
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << "Masukkan elemen matriks " << nama << " ke-" << i + 0 << "," << j + 0 << ": ";
+            cin >> matriks[i][j];
+        }
+    }
 }
 
-
-int main(){
-	float matrix[2][2];
-	char s =' '; //space
-	cout<< "Masukan nilai matriks 2x2\n";
-	for (int i = 0; i < 2; i++) // tambah elemen pada tulisan
-	// cout otomatis
-	{
-		for (int j = 0; j < 2; j++)
-		{
-			cout << "Masukan angka untuk elemen"<<s<< i+0 <<"," << j + 0 <<s << "=" <<s;
-			cin >> matrix [i][j];
-		}
-		
-	}
-	
-	float det  = matrix2x2(matrix);
-	cout<<"Derteminan matriks: " << det << endl;
-	
-	return 0;
+// Fungsi untuk menjumlahkan matriks
+void jumlahMatriks(const int matriks1[][2], const int matriks2[][2], int hasil[][2]) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            hasil[i][j] = matriks1[i][j] + matriks2[i][j];
+        }
+    }
 }
 
+int main() {
+    int matriks1[2][2], matriks2[2][2], hasil[2][2];
+
+    isiMatriks(matriks1, "pertama");
+    isiMatriks(matriks2, "kedua");
+
+    jumlahMatriks(matriks1, matriks2, hasil);
+
+    // Menampilkan hasil penjumlahan matriks
+    cout << "Hasil penjumlahan matriks:\n";
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << hasil[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
